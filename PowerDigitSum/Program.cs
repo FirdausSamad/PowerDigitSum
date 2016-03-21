4ca7ae357bf2896514b6c05c97b828fd6a7f55e8
firdaussamad <firdaussamad94@gmail.com>
2016-03-21 23:04:24 +0800

    first commit

---
+++ PowerDigitSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerDigitSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Power digit sum
+            //2 power of 15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+            //What is the sum of the digits of the number 21000?
+
+            int digit =0, num=0,sum=0;
+            digit = Convert.ToInt32(Math.Pow(2,15));
+            Console.WriteLine("Power digit sum\n");
+            while (digit>0)
+            { 
+                num = digit % 10;
+                digit = digit / 10;
+
+                sum = sum + num;
+                Console.Write("{0} + ",num);
+            }
+            Console.Write("= {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
